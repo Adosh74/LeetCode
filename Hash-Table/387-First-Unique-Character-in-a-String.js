9e6fc85ae229d032edee68924185998074fd427c
Mohamed Shebl <98131964+Adosh74@users.noreply.github.com>
2024-12-18 20:23:26 +0200

    [387] [Time Beats: 5.09%] [Memory Beats: 75.59%] - LeetPush

---
+++ Hash-Table/387-First-Unique-Character-in-a-String.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const firstUniqChar = (s) => {
+    const obj = {};
+    for (let i = 0; i < s.length; i++) {
+        if (obj[s[i]]) {
+            obj[s[i]]++;
+        } else {
+            obj[s[i]] = 1;
+        }
+    }
+
+
+    for (let i = 0; i < s.length; i++) {
+        if (obj[s[i]] === 1) {
+            return i;
+        }
+    }
+
+    return -1
+}
