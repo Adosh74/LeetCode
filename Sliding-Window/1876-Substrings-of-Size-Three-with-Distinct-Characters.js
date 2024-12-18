63429700f531db92b028874eaa42ddd05238860b
Mohamed Shebl <98131964+Adosh74@users.noreply.github.com>
2024-12-18 20:19:08 +0200

    [1876] [Time Beats: 5.72%] [Memory Beats: 97.55%] - LeetPush

---
+++ Sliding-Window/1876-Substrings-of-Size-Three-with-Distinct-Characters.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countGoodSubstrings = function (s) {
+    let result = 0;
+    for (let i = 0; i < s.length - 2; i++) {
+        if ((s[i] != s[i + 1]) && (s[i] != s[i + 2]) && s[i + 1] != s[i + 2]) {
+            result += 1
+        }
+    }
+
+    return result
+};
